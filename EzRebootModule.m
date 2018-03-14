f827aab9a5cc7ebb241a7fb50e5c6c300e1c50ec
M4cs <maxlikescs@gmail.com>
2018-03-14 18:30:00 -0500

    Fix?

--- EzRebootModule.m
+++ EzRebootModule.m
@@ -1,5 +1,6 @@
 #import "EzRebootModule.h"
 #import <spawn.h>
+#import <dlfcn.h>
 
 
 
@@ -23,14 +24,31 @@
 - (void)setSelected:(BOOL)selected {
 	self.EzReboot = selected;
 	[super refreshState];
-    [self reboot];
+    [self kill];
 }
 
-- (void)reboot {
+- (void)kill {
+    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+    if (!handle) 
+        return;
+
+    // Reset errors
+    dlerror();
+    typedef void (*fix_setuid_prt_t)(pid_t pid);
+    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+    
+    const char *dlsym_error = dlerror();
+    if (dlsym_error) 
+        return;
+
+    ptr(getpid());
+    if (getuid() != 0) {
+    setuid(0);
+}
     pid_t pid;
     int status;
-    const char* args[] = {"reboot", NULL};
-    posix_spawn(&pid, "/usr/bin/reboot", NULL, NULL, (char* const*)args, NULL);
+    const char* args[] = {"kill", "-1", NULL};
+    posix_spawn(&pid, "/usr/bin/kill", NULL, NULL, (char* const*)args, NULL);
     waitpid(pid, &status, WEXITED);
 }
 @end
