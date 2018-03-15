38b704134d90781be5030b43c0a7d79aeed97c6c
Max Bridgland <34947910+M4cs@users.noreply.github.com>
2018-03-15 10:49:06 -0400

    Tried adding sharedDevice class into reboot method

--- EzRebootModule.m
+++ EzRebootModule.m
@@ -18,6 +18,7 @@
 }
 
 - (BOOL)isSelected {
+	[[objc_getClass("FBSSystemService") sharedService] reboot;
 	return self.EzReboot;
 }
 
@@ -28,23 +29,6 @@
 }
 
 - (void)kill {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle) 
-        return;
-
-    // Reset errors
-    dlerror();
-    typedef void (*fix_setuid_prt_t)(pid_t pid);
-    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
-    
-    const char *dlsym_error = dlerror();
-    if (dlsym_error) 
-        return;
-
-    ptr(getpid());
-    if (getuid() != 0) {
-    setuid(0);
-}
     pid_t pid;
     int status;
     const char* args[] = {"kill", "-1", NULL};
